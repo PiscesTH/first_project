3a469a753695a5ce7282defd74afd0cf6e3743b6
TH <kareme007@gmail.com>
2023-12-22 15:38:13 +0900

    23.12.22.

--- src/main/java/com/clean/cleanssakssak/diary/DiaryController.java
+++ src/main/java/com/clean/cleanssakssak/diary/DiaryController.java
@@ -5,6 +5,9 @@ import com.clean.cleanssakssak.diary.model.*;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -49,5 +52,10 @@ public class DiaryController {
     @PatchMapping
     public ResVo putDiary(@RequestBody DiaryUpdDto dto) {
         return service.updDiary(dto);
+    }
+
+    @ExceptionHandler({ArrayIndexOutOfBoundsException.class, NullPointerException.class})
+    public ResponseEntity<String> handle(Exception exception){
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
