37e1270ae1f09fc4aaf0e6c1faaeef661094bf4c
TH <kareme007@gmail.com>
2024-01-03 12:37:44 +0900

    24.01.03.

--- src/main/java/com/clean/cleanssakssak/common/WebController.java
+++ src/main/java/com/clean/cleanssakssak/common/WebController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class WebController implements ErrorController { //시큐리티 사용시 이슈 발생
+public class WebController implements ErrorController { //시큐리티 사용시 이슈 발생. /허용 해야할 때 안해야 할 때 구분을 못해서 생기는 이슈 ?
     @GetMapping({"/", "/error"})
     public String index() {
         return "index.html";
